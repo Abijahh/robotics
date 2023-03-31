5a41e7d8f35094611fe04573135cc24427c8c000
Abijahh <124657902+Abijahh@users.noreply.github.com>
2023-03-31 11:11:54 +0300

    arduinocode

---
+++ Arduino/arduino/arduino.ino
@@ -0,0 +1,18 @@
+int num = 40;
+void setup() {
+serial.begin(115200);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+/*blink on LED on pin 9*/
+//set up Pinmodes(either output or input)
+pinMode(9,OUTPUT);
+digitalWrite(9,HIGH);
+
+
+
